921a1cca99fcc8bb561dac0368839a49f521ef6d
Raphaël Vandon <vandonr@gmail.com>
2021-12-02 09:35:02 +0100

    solve day2

--- Program.cs
+++ Program.cs
@@ -8,8 +8,55 @@ namespace AdventOfCode
         static void Main()
         {
             Stopwatch sw = Stopwatch.StartNew();
-            Console.WriteLine(Day1Part2(Input.Day1));
+            Console.WriteLine(Day2Part2(Input.Day2));
             Console.WriteLine($" -- {sw.ElapsedMilliseconds}ms");
+        }
+
+        static int Day2Part2(string[] instructions)
+        {
+            int x = 0, y = 0, aim = 0;
+            foreach (var i in instructions)
+            {
+                var ins = i.Split(" ");
+                var move = Int32.Parse(ins[1]);
+                switch (ins[0])
+                {
+                    case "forward":
+                        x += move;
+                        y += aim * move;
+                        break;
+                    case "up":
+                        aim -= move;
+                        break;
+                    case "down":
+                        aim += move;
+                        break;
+                }
+            }
+            return x * y;
+        }
+
+        static int Day2(string[] instructions)
+        {
+            int x = 0, y = 0;
+            foreach (var i in instructions)
+            {
+                var ins = i.Split(" ");
+                var move = Int32.Parse(ins[1]);
+                switch (ins[0])
+                {
+                    case "forward":
+                        x += move;
+                        break;
+                    case "up":
+                        y -= move;
+                        break;
+                    case "down":
+                        y += move;
+                        break;
+                }
+            }
+            return x * y;
         }
 
         static int Day1Part2(int[] depth)
