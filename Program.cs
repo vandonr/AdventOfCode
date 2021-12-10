8148c1fd8174d23602ed39cdb1a3f1e9cd10fcce
Raphaël Vandon <vandonr@gmail.com>
2021-12-10 10:12:32 +0100

    solve day 10

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace AdventOfCode
         static void Main()
         {
             Stopwatch sw = Stopwatch.StartNew();
-            Console.WriteLine(Day9.Part2(Day9.Input));
+            Console.WriteLine(Day10.Part2(Day10.Input));
             Console.WriteLine($" -- {sw.ElapsedMilliseconds}ms");
         }
     }
