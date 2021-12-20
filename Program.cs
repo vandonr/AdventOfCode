94cc414fe137d56308e63bca780f3db8f0551121
Raphaël Vandon <raphael.vandon@datadoghq.com>
2021-12-20 09:12:13 +0100

    solve day 20

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace AdventOfCode
         static void Main()
         {
             Stopwatch sw = Stopwatch.StartNew();
-            Console.WriteLine(Day18.Part2(Day18.Input));
+            Console.WriteLine(Day20.Part2(Day20.Algo, Day20.Image));
             Console.WriteLine($" -- {sw.ElapsedMilliseconds}ms");
         }
     }
