b800bbd57e13f21c33abfb641fef3693b328d9c8
Raphaël Vandon <vandonr@gmail.com>
2021-12-21 10:56:14 +0100

    day 21 part 1

---
+++ Day21.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Day21
+    {
+        public static int Part2()
+        {
+            return 0;
+        }
+
+        public static int Part1(int[] position)
+        {
+            int dicerolls = 0;
+            int dice = 0;
+            var score = new int[2];
+            while(score[0] < 1000 && score[1] < 1000)
+            {
+                var player = (dicerolls % 6) / 3;
+                position[player] = 1 + (position[player] + ThreeRollsD100(ref dice) - 1) % 10;
+                score[player] += position[player];
+                dicerolls += 3;
+            }
+            return dicerolls * score.Min();
+        }
+
+        private static int ThreeRollsD100(ref int dice)
+        {
+            var ret = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                ret += (dice % 100) + 1;
+                dice = (dice + 1) % 100;
+            }
+            return ret;
+        }
+
+        public static readonly int[] Input = new int[] {1, 10};
+    }
+}
